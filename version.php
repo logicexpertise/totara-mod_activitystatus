2cf50e453f58276f05b2b9be698eec9dbb847dae
Olumuyiwa Taiwo <muyi.taiwo@logicexpertise.com>
2022-03-11 08:55:20 +1300

    Bump version

--- version.php
+++ version.php
@@ -10,6 +10,6 @@
  */
 
 $plugin->component = 'mod_activitystatus';
-$plugin->version = 2021092201;
+$plugin->version = 2022031000;
 $plugin->requires = 2015051107;
 $plugin->maturity = MATURITY_RC;
